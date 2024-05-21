89d5e23c809c3c9e95de5a097f6cce4bdd6d03c5
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2024-05-22 01:12:34 +0400

    inital

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+export const consts = {
+  SITE_TITLE: 'Portfolio',
+  SITE_DESCRIPTION: 'Welcome to my website!',
+
+  //TODO: here declare links and mention portfolio
+  // make web like this
+  // https://tanstack.com/
+  PORTFOLIO_LINK: 'https://portfolio.beastz.dev',
+};
